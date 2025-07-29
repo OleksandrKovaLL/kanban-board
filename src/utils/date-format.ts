17e4ec759ad324b9aa246ae05e59f38e8fb23588
Oleksandr Koval <timew2762@gmail.com>
2025-07-29 12:27:28 +0300

    hot fix

--- src/utils/date-format.ts
+++ src/utils/date-format.ts
@@ -1,4 +1,4 @@
-import { format, differenceInDays } from 'date-fns'
+import { format } from 'date-fns'
 
 export const formatDate = (timestamp: number) =>
   format(new Date(timestamp), 'dd.MM.yyyy')
